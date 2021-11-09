b8af480a543a028688c95c8846dbe80fed324430
perdigao1 <perdigao1@yahoo.com>
2021-11-09 11:32:45 +0000

    Removed noarch supportl, created a script to do conda installer

---
+++ conda-create-package.bat
@@ -0,0 +1,6 @@
+REM Script file to automatically run the conda building package
+
+rmdir /s/q conda-built-packages
+mkdir conda-built-packages
+
+conda-build --output-folder ./conda-built-packages conda-recipe
