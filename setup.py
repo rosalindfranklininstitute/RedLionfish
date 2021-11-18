4b898a4c111a6ad9209e2764787400bd1938aa75
perdigao1 <perdigao1@yahoo.com>
2021-11-18 13:44:58 +0000

    updated readme

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.8',
         'Operating System :: Windows :: POSIX :: Linux :: MacOS',
+        'Framework :: napari'
     ],
     license='Apache License, Version 2.0',
     zip_safe=False,
