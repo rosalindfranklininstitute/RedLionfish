b060802834852f9d83fdb3209f34d831b5f362a1
perdigao1 <perdigao1@yahoo.com>
2021-11-23 16:16:05 +0000

    Update pip_deploy.bat

--- pip_deploy.bat
+++ pip_deploy.bat
@@ -2,13 +2,13 @@
 REM Deply package to Pypi
 
 rem updates
-python -m pip install --upgrade setuptools wheel
+python -m pip install --upgrade setuptools wheel --user
 
 rem This will create two files in /dist folder: a .whl and a .tar.zip (.gz) file with the package
 python setup.py sdist bdist_wheel
 
 rem updates
-pip install --upgrade twine
+pip install --upgrade twine --user
 
 rem Uploads ( to PyPi by default). This will ask for credentials
 twine upload dist/*
