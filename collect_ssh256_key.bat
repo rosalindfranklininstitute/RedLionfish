19ff16fe307343e417039627240224b29b4f4a95
Luis Perdigao <perdigao1@yahoo.com>
2022-01-31 13:07:03 +0000

    Create collect_ssh256_key.bat

---
+++ collect_ssh256_key.bat
@@ -0,0 +1,6 @@
+rem comand to collect ssh256
+rem run it under 'dev' conda environment
+
+rem curl -sL https://github.com/rosalindfranklininstitute/RedLionfish/archive/refs/tags/v0.5.tar.gz | openssl sha256
+
+curl -sL https://github.com/rosalindfranklininstitute/RedLionfish/archive/refs/tags/v0.6.tar.gz | openssl sha256
